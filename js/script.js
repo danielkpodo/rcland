8b87b8a7c3e2c4a225dfb90f6a20af40fd90b7dc
naphtha <naphthanewman@gmail.com>
2019-09-02 22:56:37 +0000

    removed fitvid.js and added custom css for video responsiveness

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 /* simple lightbox initialization */
 const gallery = $(".gallery a").simpleLightbox();
 
-$(document).ready(function() {
-  $("#video").fitVids();
-});
-
 AOS.init({
   duration: 1000
 });
