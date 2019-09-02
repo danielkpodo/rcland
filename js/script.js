e94011fa2423b16f39b7943ded537a020cd60513
naphtha <naphthanewman@gmail.com>
2019-09-02 21:48:38 +0000

    added animation scroll js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const gallery = $(".gallery a").simpleLightbox();
 $(document).ready(function() {
   $("#video").fitVids();
 });
+
+AOS.init({
+  duration: 1000
+});
+
+const review = document.getElementById("review");
+
+review.addEventListener("click", function() {
+  jump(".review");
+});
